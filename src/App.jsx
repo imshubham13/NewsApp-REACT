127db9dd5bcf01daa7e20716854927af74041730
imshubham13 <shubhamsavaliya13@gmail.com>
2024-12-31 10:03:38 +0530

    News-App using api with ReactJS commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import NavBar from "./Component/NavBar";
+import NewsBoard from "./Component/NewsBoard";
+
+function App() {
+  const [category, setCategory] = useState("general");
+  return (
+    <>
+      <NavBar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+    </>
+  );
+}
+
+export default App;
